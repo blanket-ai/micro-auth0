426ae659f0e11a9f5e50bad1a2fc8442eb63b9fc
Mark Marijnissen <markmarijnissen@gmail.com>
2017-07-13 18:04:06 +0200

    0.4.0 use request library instead of auth0 library

--- README.md
+++ README.md
@@ -12,12 +12,10 @@ npm install --save micro-auth0
 
 ```js
 const { send } = require('micro');
-const auth0 = require('micro-auth0')({
-  domain: process.env.AUTH0_DOMAIN
-});
+const auth0 = require('micro-auth0');
 
 module.exports = async (req,res) => {
-  const user = await auth0(req);
+  const user = await auth0(req, process.env.AUTH0_DOMAIN);
   if(!user) return send(res,403,{ error: "Forbidden" });
   return {
       date: new Date(),
@@ -27,5 +25,7 @@ module.exports = async (req,res) => {
 ```
 ## changelog
 
+- 0.4.0 refactor to user request library
+- 0.3.0 refactor to NOT use the Auth0 library
 - 0.2.0 add async-to-gen to support node 6
 - 0.1.0 initial version
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var url = require('url');
-const AuthenticationClient = require('auth0').AuthenticationClient;
+var url = require("url");
+var request = require("request");
 
-module.exports = function createMicroAuth0(options) {
-  var auth0 = new AuthenticationClient(options);
-
-  return async (req) => {
-    const query = url.parse(req.url, true).query || {};
+module.exports = (req,domain) => new Promise((resolve,reject) => {
+    const query = url.parse(req.url || "", true).query || {};
     let token = query.token || "";
-    if(req.headers.authorization && req.headers.authorization.match(/bearer /i)){
-      token = req.headers.authorization.substr(7);
+    if(req.headers.authorization && req.headers.authorization.match(/bearer /i)) {
+        token = req.headers.authorization.substr(7);
     }
-    try {
-      const result = await auth0.getProfile(token);
-      if(result === "Unauthorized") return false;
-      return JSON.parse(result);
-    } catch(error) {
-      return false;
-    }
-  }
-};
+    request({
+        url: `https://${domain}/userinfo`,
+        method: "GET",
+        headers: { "Authorization": `Bearer ${token}` }
+    }, (err, res, result) => {
+        if (err || result === "Unauthorized" || res.statusCode !== 200) {
+            resolve(false);
+        } else {
+            resolve(JSON.parse(result));
+        }
+    });
+});
